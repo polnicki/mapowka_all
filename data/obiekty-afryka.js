5ddfb29a4c553a4273abe5fdfdd636992df638bc
Rafal Polnicki <rpo02@hrs.com>
2025-10-08 13:48:10 +0200

    dodano - afryka, ameryka, autralia

---
+++ data/obiekty-afryka.js
@@ -0,0 +1,74 @@
+window.obiekty = [
+    // OCEANY I MORZA
+    ["Ocean Atlantycki", -5, -15, "Ocean"],
+    ["Ocean Indyjski", -20, 50, "Ocean"],
+    ["Morze Czerwone", 20, 40, "Morze"],
+    ["Morze Śródziemne", 35, 18, "Morze"],
+    // ZATOKI I CIEŚNINY
+    ["Zatoka Gwinejska", 0, 0, "Zatoka"],
+    ["Zatoka Adeńska", 12, 48, "Zatoka"],
+    ["Wielka Syrta", 32, 18, "Zatoka"],
+    ["Mała Syrta", 34, 20, "Zatoka"],
+    ["Cieśnina Gibraltarska", 36, -5, "Cieśnina"],
+    ["Cieśnina Bab al-Mandab", 12.5, 43.5, "Cieśnina"],
+    ["Kanał Sueski", 30, 32.5, "Kanał"],
+    ["Kanał Mozambicki", -18, 41, "Kanał"],
+    // WYSPY I ARCHIPELAGI
+    ["Madagaskar", -20, 47, "Wyspa"],
+    ["Sokotra", 12.5, 54, "Wyspa"],
+    ["Komory", -12, 44, "Wyspa"],
+    ["Seszele", -4.6, 55.5, "Wyspa"],
+    ["Wyspy Zielonego Przylądka", 16, -24, "Wyspa"],
+    ["Wyspy Kanaryjskie", 28, -16, "Wyspa"],
+    ["Wyspy Świętego Tomasza i Książęca", 0.2, 6.6, "Wyspa"],
+    // PRZYLĄDKI
+    ["Przylądek Biały (zachodni)", 21.5, -17, "Przylądek"],
+    ["Przylądek Biały (północny)", 37, 9, "Przylądek"],
+    ["Przylądek Zielony", 16, -24, "Przylądek"],
+    ["Przylądek Hafun", 10, 51.5, "Przylądek"],
+    ["Przylądek Igielny", -34.8, 20, "Przylądek"],
+    ["Przylądek Dobrej Nadziei", -34.4, 18.5, "Przylądek"],
+    // PÓŁWYSPY
+    ["Półwysep Somalijski", 6, 49, "Półwysep"],
+    ["Półwysep Barka (Cyrenajka)", 32, 22, "Półwysep"],
+    // RZEKI
+    ["Nil", 30, 32, "Rzeka"],
+    ["Zambezi", -15, 34, "Rzeka"],
+    ["Orange", -29, 23, "Rzeka"],
+    ["Okawango", -19, 23, "Rzeka"],
+    ["Limpopo", -23, 32, "Rzeka"],
+    ["Kongo", -1, 15, "Rzeka"],
+    ["Niger", 10, 5, "Rzeka"],
+    ["Senegal", 14, -16, "Rzeka"],
+    // JEZIORA
+    ["Jezioro Czad", 13, 14, "Jezioro"],
+    ["Jezioro Wolta", 6, -1, "Jezioro"],
+    ["Jezioro Nasera", 23.7, 33, "Jezioro"],
+    ["Jezioro Wiktorii", -1, 33, "Jezioro"],
+    ["Jezioro Turkana", 3, 36, "Jezioro"],
+    ["Jezioro Malawi", -12, 34, "Jezioro"],
+    ["Jezioro Tanganika", -5, 29, "Jezioro"],
+    ["Jezioro Kariba", -17, 28.5, "Jezioro"],
+    // ROWY
+    ["Wielki Rów Wschodni", -3, 35, "Rów"],
+    ["Wielki Rów Zachodni", 2, 30, "Rów"],
+    ["Rów Abisyński", 9, 39, "Rów"],
+    // KOTLINY
+    ["Kotlina Czadu", 14, 17, "Kotlina"],
+    ["Kotlina Konga", -2, 23, "Kotlina"],
+    ["Kotlina Kalahari", -22, 24, "Kotlina"],
+    ["Kotlina Nilu Białego", 7, 31, "Kotlina"],
+    // WYŻYNY
+    ["Wyżyna Dżos", 9, 9, "Wyżyna"],
+    ["Wyżyna Somalijska", 8, 45, "Wyżyna"],
+    ["Wyżyna Abisyńska", 10, 39, "Wyżyna"],
+    ["Wyżyna Mitumba", -4, 29, "Wyżyna"],
+    ["Wyżyna Bije", -12, 22, "Wyżyna"],
+    ["Wyżyna Wschodnioafrykańska", -1, 36, "Wyżyna"],
+    // GÓRY
+    ["Góry Atlas", 32, -4, "Góry"],
+    ["Góry Przylądkowe", -34, 19, "Góry"],
+    ["Góry Smocze", -28, 30, "Góry"],
+    ["Góry Adamawa", 7, 13, "Góry"],
+    ["Góry Darfur", 13, 24, "Góry"]
+];
---
+++ data/obiekty-australia.js
@@ -0,0 +1,62 @@
+window.obiekty = [
+    // OCEANY
+    ["Ocean Indyjski", -30, 110, "Ocean"],
+    ["Ocean Spokojny", -20, 155, "Ocean"],
+    // MORZA
+    ["Morze Tasmana", -40, 155, "Morze"],
+    ["Morze Koralowe", -18, 150, "Morze"],
+    ["Morze Salomona", -8, 156, "Morze"],
+    ["Morze Bismarcka", -3, 147, "Morze"],
+    ["Morze Arafura", -10, 135, "Morze"],
+    ["Morze Timor", -10, 128, "Morze"],
+    // ZATOKI
+    ["Zatoka Karpentaria", -14, 140, "Zatoka"],
+    ["Wielka Zatoka Australijska", -33, 131, "Zatoka"],
+    // CIEŚNINY
+    ["Cieśnina Bassa", -39, 146, "Cieśnina"],
+    ["Cieśnina Torresa", -10.5, 143.5, "Cieśnina"],
+    // INNE
+    ["Wielka Rafa Koralowa", -16, 147, "Rafa"],
+    // WYSPY I ARCHIPELAGI
+    ["Nowa Zelandia", -41, 174, "Wyspa"],
+    ["Tasmania", -42, 147, "Wyspa"],
+    ["Nowa Gwinea", -6, 143, "Wyspa"],
+    ["Nowe Hebrydy", -16, 167, "Wyspa"],
+    ["Nowa Kaledonia", -21, 165, "Wyspa"],
+    ["Wyspy Salomona", -9, 160, "Wyspa"],
+    ["Archipelag Bismarcka", -4, 150, "Archipelag"],
+    // PRZYLĄDKI
+    ["Przylądek Południowy", -35, 138.1, "Przylądek"],
+    ["Przylądek Byrona", -28.6, 153.6, "Przylądek"],
+    ["Przylądek Jork", -10.7, 142.5, "Przylądek"],
+    ["Przylądek Steep", -34.4, 117.7, "Przylądek"],
+    // PÓŁWYSPY I ZIEMIE
+    ["Półwysep Ptasia Głowa", -8, 143, "Półwysep"],
+    ["Półwysep Jork", -11, 143, "Półwysep"],
+    ["Półwysep Eyrego", -32, 135, "Półwysep"],
+    ["Ziemia Arnhema", -12, 134, "Obszar"],
+    // RZEKI
+    ["Rzeka Darling", -31, 145, "Rzeka"],
+    ["Rzeka Murray", -35, 140, "Rzeka"],
+    // JEZIORA
+    ["Jezioro Eyre", -28.3, 137.5, "Jezioro"],
+    // NIZINY I WYŻYNY
+    ["Nizina Nullarbor", -31, 129, "Nizina"],
+    ["Wyżyna Barkly", -19, 137, "Wyżyna"],
+    // GÓRY
+    ["Góry Hamersley", -22, 119, "Góry"],
+    ["Góry Macdonnella", -23.5, 133.5, "Góry"],
+    ["Góry Musgrave", -27, 131, "Góry"],
+    ["Góry Kimberley", -17, 127, "Góry"],
+    ["Wielkie Góry Wododziałowe", -29, 152, "Góry"],
+    ["Alpy Południowe", -45, 167, "Góry"],
+    ["Alpy Australijskie", -36, 148, "Góry"],
+    ["Góra Kościuszki", -36.45, 148.26, "Szczyt"],
+    // PUSTYNIE
+    ["Wielka Pustynia Piaszczysta", -20, 125, "Pustynia"],
+    ["Wielka Pustynia Wiktorii", -28, 130, "Pustynia"],
+    ["Pustynia Gibsona", -25, 127, "Pustynia"],
+    ["Pustynia Simpsona", -24, 138, "Pustynia"],
+    // BASEN
+    ["Wielki Basen Artezyjski", -25, 145, "Basen"]
+];
